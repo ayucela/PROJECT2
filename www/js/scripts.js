86e74808529eaed6dc43ee374cff59b92984f3cf
Vladimir Morozov <freezer278@gmail.com>
2017-06-03 13:10:39 +0300

    small improvement for returning with back button

--- www/js/scripts.js
+++ www/js/scripts.js
@@ -113,7 +113,9 @@ tjq(document).ready(function() {
         }
     });
 
-    tjq("#mainsearchform-date_to").datepicker('disable');
+    if (tjq("#mainsearchform-date_to").val() === '') {
+        tjq("#mainsearchform-date_to").datepicker('disable');
+    }
 
     tjq(document).on('change', 'input#mainsearchform-date_from', function() {
         tjq("#mainsearchform-date_to").datepicker('enable');
